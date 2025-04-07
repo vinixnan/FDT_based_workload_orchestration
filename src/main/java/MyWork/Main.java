2ab5e9e935c05ab956aa31c94c7fe98147f7c116
Vinicius Renan <vinyciux@gmail.com>
2025-04-07 16:26:22 -0300

    Add maven

---
+++ src/main/java/MyWork/Main.java
@@ -0,0 +1,29 @@
+package MyWork;
+ 
+import com.mechalikh.pureedgesim.simulationmanager.Simulation;
+
+public class Main{
+	// Below is the path for the settings folder of this example
+	private static String settingsPath = "settings/";
+
+	// The custom output folder is
+	private static String outputPath = "output/";
+
+	public static void main(String[] args) {
+		
+		Simulation sim= new Simulation();
+		// changing the default output folder
+		sim.setCustomOutputFolder(outputPath);
+
+		// changing the simulation settings folder
+		sim.setCustomSettingsFolder(settingsPath);
+
+		// Load the custom devices class
+		sim.setCustomEdgeDataCenters(CustomComputingNode.class); 
+		
+	    sim.setCustomEdgeOrchestrator(CustomOrchestrator.class);
+		// Launch the simulation
+		sim.launchSimulation();
+	}
+
+}
